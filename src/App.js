fc5f36d6ff07cff1c8270cdfe977095010b40a35
Slin Castro <scastro@thoughtworks.com>
2020-04-26 00:13:54 -0500

    remove test data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 import CardList from './card/Card'
 import Form from './form/Form'
 
-
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
-
 class App extends React.Component {
 
   state= {
     profiles : []
   };
-  
+
   addNewProfile = (profileData) => {
     this.setState(prevState => ({
       profiles: [...prevState.profiles, profileData]
