7f19fdbc93c1a4e339af988b50dcbff3308b189a
Slin Castro <scastro@thoughtworks.com>
2020-04-25 20:19:41 -0500

    include components class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardList from './card/Card'
+import Form from './form/Form'
 
-function App() {
-  return (
+
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+class App extends React.Component {
+
+  state= {
+    profiles : testData
+  };
+
+  render(){
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">"Prueba de concepto"</div>
+        <Form/>
+        <CardList profiles={this.state.profiles}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Form extends React.Component{
+    render(){
+        return(
+            <form action="">
+                <input placeholder="Github user"></input>
+                <button>Add User</button>
+            </form>
+        );
+    }
+}
+
+export default Form
