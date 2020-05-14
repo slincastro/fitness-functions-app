e02d8a3ca916ec4cd905b5a7611cb2171a801607
Slin Castro <scastro@thoughtworks.com>
2020-05-14 11:01:14 -0500

    extract elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Stars from './starsMatch/stars'
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Stars />
   </React.StrictMode>,
   document.getElementById('root')
 );
